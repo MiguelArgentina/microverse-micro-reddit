94405faad101f966aeb6d75e58a990271d7e9675
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-10 19:04:20 -0300

    Add association with comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   validates :title, presence: true, length: {minimum: 2, maximum: 50}
   validates :body, presence: true, length: {minimum: 2, maximum: 200}
+  validates :user_id, presence: true
 end
