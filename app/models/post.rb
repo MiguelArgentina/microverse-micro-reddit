c4dd4f3b4292817d0b0685a0bf7ddc99b7dd9788
George <71434803+george-swift@users.noreply.github.com>
2021-03-10 22:20:18 +0100

    Add association to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 2, maximum: 50}
   validates :body, presence: true, length: {minimum: 2, maximum: 200}
-
-  belongs_to:user
 end
