cc84568062c247e590c350ec73421dbbf6053e3f
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-10 18:03:38 -0300

    Add association to Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  
   validates :name, presence: true, length: {minimum: 5, maximum: 20}
   validates :nickname, presence: true, length: {minimum: 5, maximum: 20}, uniqueness: true
 end
