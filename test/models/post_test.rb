06e1ec7824b311c28a7a135277811833109e4d92
George <71434803+george-swift@users.noreply.github.com>
2021-03-10 22:20:43 +0100

    Add test for a user's post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.first
+    @post = @user.posts.build(title: 'Lorem ipsum', body: 'Lorem ipsum Lorem ipsum Lorem ipsum')
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
 end
