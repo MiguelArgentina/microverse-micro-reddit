500112a5045a724b24ca7113f1eadecd5d7e4bd3
George <71434803+george-swift@users.noreply.github.com>
2021-03-10 22:21:29 +0100

    Add test to validate post references user

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -9,4 +9,11 @@ class PostTest < ActiveSupport::TestCase
   test 'should be valid' do
     assert @post.valid?
   end
+
+  test 'user id should be present' do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  
 end
